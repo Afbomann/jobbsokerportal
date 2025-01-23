09996cc43d02b6974b0851e457c87c5288dbf604
Afbomann <178702842+Afbomann@users.noreply.github.com>
2025-01-23 12:28:38 +0100

    Update authentication.tsx

--- src/libs/authentication.tsx
+++ src/libs/authentication.tsx
@@ -1,19 +1,19 @@
-import jsonwebtoken from "jsonwebtoken"
-import { cookies } from "next/headers"
+import jsonwebtoken from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export async function authenticate(): Promise<boolean> {
-    const cookies_ = await cookies();
-    const cookie = cookies_.get("token");
+  const cookies_ = await cookies();
+  const cookie = cookies_.get("token");
 
-    if (!cookie) return false;
+  if (!cookie) return false;
 
-    return await new Promise((resolve) => {
-        jsonwebtoken.verify(cookie.value, process.env.ADMIN_JWT_SECRET!, (err, decoded) => {
-            if (err) return resolve(false);
+  return await new Promise((resolve) => {
+    jsonwebtoken.verify(cookie.value, process.env.ADMIN_JWT_SECRET!, (err) => {
+      if (err) return resolve(false);
 
-            return resolve(true);
-        })
+      return resolve(true);
+    });
 
-        resolve(false);
-    })
+    resolve(false);
+  });
 }
