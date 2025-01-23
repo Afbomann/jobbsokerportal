823acaf4efc96b3361855a888dcd6e599c7debbd
Afbomann <178702842+Afbomann@users.noreply.github.com>
2025-01-23 20:48:48 +0100

    feat: added opengraph image to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
   title: "Jobbsøkerportal - Tiller VGS",
   description:
     "Dette er en portal som er utviklet for å gi deg oversikt over tilgjengelige utlysninger. Her kan du enkelt finne oppdaterte søknadsutlysninger og muligheter for å starte eller utvikle karrieren din innen ulike fagområder.",
+  openGraph: { images: ["https://jobbsokerportal.vercel.app/images/bg.jpg"] },
 };
 
 export default function RootLayout({
