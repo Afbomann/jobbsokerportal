de0179266988216f8bf0d39fbbe1c14f2047bcea
Afbomann <178702842+Afbomann@users.noreply.github.com>
2025-01-23 12:46:16 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { prisma } from "@/libs/prisma";
 
+export const revalidate = 10;
+
 export default async function HomePage() {
   const applications = await prisma.application.findMany({
     orderBy: { id: "desc" },
